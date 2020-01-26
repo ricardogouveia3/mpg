177b3625344bb142d42446a78d5c3ecda97e0901
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2020-01-26 10:00:00 -0300

    Fetch from RCRD API

--- js/generator.js
+++ js/generator.js
@@ -1,10 +1,6 @@
-// Full
-// const en = 'https://raw.githubusercontent.com/ricardogouveia3/genpass/master/data/words_dictionary_en.json';
-// const br = 'https://raw.githubusercontent.com/ricardogouveia3/genpass/master/data/words_dictionary_br.json';
-
-// Reduce
-const en = 'https://raw.githubusercontent.com/ricardogouveia3/mpg/master/data/words_reduce_en.json';
-const br = 'https://raw.githubusercontent.com/ricardogouveia3/mpg/master/data/words_reduce_br.json';
+// Dics
+const en = 'https://api.rcrd.me/mpg/en';
+const br = 'https://api.rcrd.me/mpg/br';
 
 
 let selectedDic = 'en';
@@ -38,7 +34,7 @@ generateButton.addEventListener('click', function (event) {
 })
 
 function changeDic() {
-  if ( selectedDic === 'en' ) { selectedDic = 'br'; }
+  if (selectedDic === 'en') { selectedDic = 'br'; }
   else { selectedDic = 'en'; }
 }
 
@@ -59,7 +55,7 @@ function manageLastFive() {
   for (pass of lastFiveGeneratedPasses) {
     let mobileRow = lastFiveTableDesktop.insertRow();
     let desktopRow = lastFiveTableMobile.insertRow();
-    
+
     let desktopCell = desktopRow.insertCell();
     let mobileCell = mobileRow.insertCell();
 
@@ -84,14 +80,14 @@ function initializeDic() {
   words.then(
     response => response.json()).then(
       arrayOfWords => {
-        const words = Object.keys(arrayOfWords);
+        const words = arrayOfWords;
 
         validLengthWords = words.filter((word) => {
           if (word.length >= 3 && word.length <= 8 && !word.contai) { return true; } else { return false; }
         });
 
-        validLengthWords = validLengthWords.map((word) => {return word.replace(/[1-9]/g, '');})
-        validLengthWords = validLengthWords.map((word) => {return word.replace(/[1-9]/g, '');})
+        validLengthWords = validLengthWords.map((word) => { return word.replace(/[1-9]/g, ''); })
+        validLengthWords = validLengthWords.map((word) => { return word.replace(/[1-9]/g, ''); })
 
         if (firstRun) {
           firstRun = false;
@@ -106,7 +102,7 @@ function generatePass() {
   numberOfComponents = +numberOfComponents;
 
   for (var i = 0; i < numberOfComponents; i++) {
-    if ( i != numberOfComponents - 1) {
+    if (i != numberOfComponents - 1) {
       generatedPass += randomWord() + selectedSeparator;
     } else { generatedPass += randomInt(); }
   }
@@ -114,7 +110,7 @@ function generatePass() {
 }
 
 function updatePreferences() {
-  if ( selectedDic !== lastSelectedDic ) { initializeDic(); }
+  if (selectedDic !== lastSelectedDic) { initializeDic(); }
 
   selectedSeparator = document.querySelector('#selectedSeparator').value;
   numberOfComponents = document.querySelector('#numberOfComponents').value;
